b55403c7c6ffa00a3ddda9e8ff9079e0f0140662
vijaykrishna549 <pvijaykrishna549@gmail.com>
2023-06-21 00:31:28 +0530

    Added use case 3 to 9 for regex validattion and refactored code

--- main/java/regex_excercise/RegexMain.java
+++ main/java/regex_excercise/RegexMain.java
@@ -11,9 +11,51 @@ public class RegexMain {
         System.out.println(num.replaceAll("([1-9]{6})","Correct Pin-code"));
 
 //UC 1 ->    As a User need to enter a valid First Name , First name starts with Cap and has minimum 3 characters
-        System.out.println(Pattern.matches("^[A-Z][a-z]{3,}$", "Vijay"));
+        boolean isFirstNameRegexCorrect = Pattern.matches("^[A-z][a-z]{3,}$", "Vijay");
+        if(isFirstNameRegexCorrect){
+            System.out.println("Regex for first name is : "+ isFirstNameRegexCorrect);
+        }
+        else {
+            System.out.println("Regex for first name is : "+ isFirstNameRegexCorrect);
+        }
+        System.out.println();
 //UC 2 -> As a User need to enter a valid Last Name - Last name starts with Cap and has minimum 3 characters
-        System.out.println(Pattern.matches("^[A-Z][a-z]{3,}\\s[A-Z][a-z]{3,}$","Vijay Krishna"));
+        boolean isFullNameRegexCorrect =Pattern.matches("^[A-Z][a-z]{3,}\\s[A-Z][a-z]{3,}$","Vijay Krishna");
+        if(isFullNameRegexCorrect){
+            System.out.println("Regex for full name is : "+ isFullNameRegexCorrect);
+        }
+        else {
+            System.out.println("Regex for full name is : "+ isFullNameRegexCorrect);
+        }
+        System.out.println();
 //UC 3 -> As a User need to enter a valid email- E.g. abc.xyz@bl.co.in - Email has 3 mandatory parts (abc, bl& co) and 2 optional (xyz & in) with precise @ and . positions
+        boolean isEmailRegexCorrect = Pattern.matches("^[a-z]{3}[.][xyz]*[@][a-z]{2}[.][co]{2}[.][in]*$","abc.xyz@bl.co.in");
+        if(isEmailRegexCorrect){
+            System.out.println("Regex for email is : "+ isEmailRegexCorrect);
+        }
+        else {
+            System.out.println("Regex for email is : "+ isEmailRegexCorrect);
+        }
+        System.out.println();
+
+//UC 4 -> As a User need to follow pre -defined Mobile Format - E.g. 91 9919819801 - Country code follow by space and 10 digit number
+        boolean isMobileNumberRegexCorrect = Pattern.matches("^([1-9]{2}[- ]?[1-9]{1}[0-9]{9})$","91 9834671094");
+        if(isMobileNumberRegexCorrect){
+            System.out.println("Regex for mobile is : "+ isMobileNumberRegexCorrect);
+        }
+        else {
+            System.out.println("Regex for mobile number is : "+ isMobileNumberRegexCorrect);
+        }
+        System.out.println();
+
+//UC 5-9 -> Password validation with (a) minimum 8 characters, at least 1 uppercase, at least 1 numeric and exactly one special character
+        boolean isPasswordRegexCorrect =Pattern.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@!#]*)[a-zA-Z0-9@!#*]{8,}", "@erttyR34");
+        if(isPasswordRegexCorrect){
+            System.out.println("Regex for password is : "+ isPasswordRegexCorrect);
+        }
+        else {
+            System.out.println("Regex for password is : "+ isPasswordRegexCorrect);
+        }
+        System.out.println();
     }
 }
